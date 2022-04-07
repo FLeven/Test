79456ec36e3cf5f828831989ea235f44df5eb811
Gitea <gitea@fake.local>
2022-04-07 22:12:05 +0200

    -

--- Test/Private/GetHelloWorld.ps1
+++ Test/Private/GetHelloWorld.ps1
@@ -1,3 +1,13 @@
 function GetHelloWorld {
+        <#
+    .SYNOPSIS
+        Returns Hello world
+    .DESCRIPTION
+        Returns Hello world
+    .EXAMPLE
+        PS> GetHelloWorld
+
+        Runs the command
+    #>
     'Hello world'
 }
