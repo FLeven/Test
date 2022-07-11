514ed9bad8ed33408617f0f9f1b8b9de4e6ffcca
Felix Leven <28837195+FLeven@users.noreply.github.com>
2022-07-11 18:47:07 +0200

    -

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,11 +1,9 @@
 properties {
     # Set this to $true to create a module with a monolithic PSM1
-    $configuration.Run.PassThru            = $true
     $PSBPreference.Build.CompileModule = $false
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
     $PSBPreference.Test.CodeCoverage.Enabled  = $true
-    $PSBPreference.Test.CodeCoverage.OutputFileFormat = 'JaCoCo'
     $PSBPreference.Test.CodeCoverage.Threshold = .00
     $PSBPreference.Test.CodeCoverage.Files = @('..\Test\Private\*.ps1', '..\Test\Public\*.ps1')
 }
